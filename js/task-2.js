b1ac0aba7c86fa44e61c043358b06d82639f454d
vatslavvolos <145980654+vatslavvolos@users.noreply.github.com>
2024-02-17 12:53:39 +0100

    df
    f

---
+++ js/task-2.js
@@ -0,0 +1,21 @@
+class Storage {
+  #items;
+  constructor(items) {
+    this.#items = items;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    return this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+  }
+}
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
